f2f38d295eaf11221a8e555738f66e26bdf53a32
Eric Sciple <ersciple@microsoft.com>
2016-01-24 02:44:43 -0500

    .

--- Tests/L0/PublishSymbols/Lock-Semaphore.RetriesOnException.ps1
+++ Tests/L0/PublishSymbols/Lock-Semaphore.RetriesOnException.ps1
@@ -4,16 +4,14 @@ param()
 # Arrange.
 . $PSScriptRoot\..\..\lib\Initialize-Test.ps1
 . $PSScriptRoot\..\..\..\Tasks\PublishSymbols\PublishHelpers\SemaphoreFunctions.ps1
+Register-Mock Test-Path { Unregister-Mock Test-Path ; throw (New-Object System.IO.IOException('Some IO exception message'))}
 Register-Mock Write-Warning
+Register-Mock Start-Sleep
 Write-Verbose ($share = [System.IO.Path]::Combine($env:TMP, ([System.IO.Path]::GetRandomFileName())))
 $semaphorePath = "$share\_lockfile.sem"
 try {
-    $null = [System.IO.Directory]::CreateDirectory($share)
-    [System.IO.File]::WriteAllText($semaphorePath, 'Some already existing semaphore file')
-    [System.IO.File]::SetCreationTime($semaphorePath, ([datetime]::Now.AddHours(-25)))
-
     # Act.
-    $semaphore = Lock-Semaphore -Share $share -MaximumWaitTime ([timespan]::FromSeconds(1)) -SemaphoreMessage "Some new semaphore message"
+    $semaphore = Lock-Semaphore -Share $share -MaximumWaitTime ($script:sleepInterval + [timespan]::FromSeconds(1)) -SemaphoreMessage "Some semaphore message"
 
     # Assert.
     Assert-AreEqual "$($semaphore.Path)" $semaphorePath
@@ -25,10 +23,10 @@ try {
             ([System.IO.FileShare]::ReadWrite))
     } -MessagePattern *file*being*used*another*process*
     $semaphore.Stream.Dispose()
-    Assert-AreEqual "Some new semaphore message" ([System.IO.File]::ReadAllText($semaphorePath))
-    Assert-WasCalled Write-Warning -Times 2
-    Assert-WasCalled Write-Warning -ArgumentsEvaluator { $args[0] -like '*SemaphoreFile0Minutes1AttemptingCleanup*' }
-    Assert-WasCalled Write-Warning -ArgumentsEvaluator { $args[0] -like '*CleanedUpSemaphoreFile0*' }
+    Assert-AreEqual "Some semaphore message" ([System.IO.File]::ReadAllText($semaphorePath))
+    Assert-WasCalled Write-Warning -Times 1
+    Assert-WasCalled Write-Warning -ArgumentsEvaluator { $args[0] -like '*Error0AccessingSemaphoreFile1Retrying2Seconds*' }
+    Assert-WasCalled Start-Sleep -Times 1 -- -Seconds $script:sleepInterval.TotalSeconds
 } finally {
     if ($semaphore.Stream) { $semaphore.Stream.Dispose() }
     if (Test-Path -LiteralPath $share) { Remove-Item -LiteralPath $share -Recurse }
