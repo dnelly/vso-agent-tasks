38650d1f9bfa94d1e31d4c1d8233325acd2a3335
Eric Sciple <ersciple@microsoft.com>
2016-01-24 04:14:49 -0500

    .

--- Tests/L0/PublishSymbols/Invoke-PublishSymbols.Publishes.ps1
+++ Tests/L0/PublishSymbols/Invoke-PublishSymbols.Publishes.ps1
@@ -6,17 +6,17 @@ param()
 . $PSScriptRoot\..\..\lib\Initialize-Test.ps1
 . $PSScriptRoot\..\..\..\Tasks\PublishSymbols\PublishHelpers\CommonFunctions.ps1
 . $PSScriptRoot\..\..\..\Tasks\PublishSymbols\PublishHelpers\PublishFunctions.ps1
-Write-Verbose ($script:share = [System.IO.Path]::Combine($env:TMP, ([System.IO.Path]::GetRandomFileName())))
+Write-Verbose ($share = [System.IO.Path]::Combine($env:TMP, ([System.IO.Path]::GetRandomFileName())))
 $pdbFiles = 'Some PDB file 1', 'Some PDB file 2'
-$script:product = 'Some product'
-$script:version = 'Some version'
+$product = 'Some product'
+$version = 'Some version'
 $maximumWaitTime = [timespan]::FromSeconds(2)
 $semaphoreMessage = 'Some semaphore message'
 $artifactName = 'Some artifact name'
-$script:responseFile = "$share\responseFile.txt"
-Register-Mock New-ResponseFile { [System.IO.File]::WriteAllText($script:responseFile, 'Some response file content') ; $script:responseFile }
-$script:semaphore = New-Object psobject
-Register-Mock Lock-Semaphore { $script:semaphore }
+$responseFile = "$share\responseFile.txt"
+Register-Mock New-ResponseFile { [System.IO.File]::WriteAllText($responseFile, 'Some response file content') ; $responseFile }
+$semaphore = New-Object psobject
+Register-Mock Lock-Semaphore { $semaphore }
 Register-Mock Get-SymStorePath { 'Some path to symstore.exe'}
 Register-Mock Invoke-VstsTool
 Register-Mock Get-LastTransactionId { 'Some last transaction ID' }
@@ -32,12 +32,12 @@ try {
     # Assert.
     Assert-WasCalled New-ResponseFile -- -PdbFiles $pdbFiles
     Assert-WasCalled Lock-Semaphore -Share $share -MaximumWaitTime ([timespan]::FromMinutes(1)) -SemaphoreMessage $semaphoreMessage
-    Assert-WasCalled Invoke-VstsTool -- -FileName 'Some path to symstore.exe' -Arguments "add /f ""@$script:responseFile"" /s ""$script:share"" /t ""$script:product"" /v ""$script:version""" -WorkingDirectory ([System.IO.Path]::GetTempPath())
+    Assert-WasCalled Invoke-VstsTool -- -FileName 'Some path to symstore.exe' -Arguments "add /f ""@$responseFile"" /s ""$share"" /t ""$product"" /v ""$version""" -WorkingDirectory ([System.IO.Path]::GetTempPath())
     Assert-WasCalled Unlock-Semaphore -- $semaphore
     Assert-WasCalled Get-ArtifactName -- -ArtifactName $artifactName -LastTransactionId 'Some last transaction ID'
     Assert-WasCalled Write-VstsAssociateArtifact -ParametersEvaluator {
         $Name -eq 'Some different artifact name' -and
-        $Path -eq $script:share -and
+        $Path -eq $share -and
         $Type -eq 'SymbolStore' -and
         $Properties['TransactionId'] -eq 'Some last transaction ID'
     }
